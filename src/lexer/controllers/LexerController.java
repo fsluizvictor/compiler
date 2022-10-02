9b09247616948fd4c28eea779701d536ca91ea2c
fsluizvictor <fs.luizvictor@gmail.com>
2022-10-01 23:20:01 -0300

    feat : add a eof

--- src/lexer/controllers/LexerController.java
+++ src/lexer/controllers/LexerController.java
@@ -3,75 +3,68 @@ package lexer.controllers;
 import java.util.Scanner;
 
 import lexer.services.LexerService;
+import lexer.models.EndOfFile;
 import lexer.models.Token;
 
 public class LexerController {
     public static void main(String[] args) {
 
         String fullPath = "/home/victor/Projects/compiler/tests/lexerTests/";
-
         LexerService service = null;
+        int testNumber = 1;
 
-        while (true) {
+        System.out.println("Temos 6 arquivos de teste.\n" +
+                "Os números de 1 a 6 se referem respectivamente a cada teste\n" +
+                "Digite o número do teste que deseja ser compilado : ");
 
-            System.out.println("Temos 6 arquivos de teste.\n" +
-                    "Os números de 1 a 6 se referem respectivamente a cada teste\n" +
-                    "Digite o número do teste que deseja ser compilado : ");
+        Scanner input = new Scanner(System.in);
+        testNumber = input.nextInt();
 
-            Scanner input = new Scanner(System.in);
-            int testNumber = input.nextInt();
-
-            switch (testNumber) {
-                case 1:
-                    fullPath += "test1.txt";
-                    break;
-                case 2:
-                    fullPath += "test2.txt";
-                    break;
-                case 3:
-                    fullPath += "test3.txt";
-                    break;
-                case 4:
-                    fullPath += "test4.txt";
-                    break;
-                case 5:
-                    fullPath += "test5.txt";
-                    break;
-                case 6:
-                    fullPath += "test6.txt";
-                    break;
-                default:
-                    break;
-            }
-
-            try {
-                service = new LexerService(fullPath);
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-
-            System.out.println("Reconhecimento dos tokens");
-
-            while (true) {
-                try {
-                    Token token = service.scan();
-                    if (token != null) {
-                        System.out.println(token.toString());
-                    } else {
-                        System.out.println("EOF");
-                        break;
-                    }
-                } catch (Exception e) {
-                    // TODO: handle exception
-                    System.out.println("Error with read file!");
-                    break;
-                }
-            }
-
-            System.out.println("Tabela de Símbolos : ");
-            service.showSymbolsTable();
-
+        switch (testNumber) {
+            case 1:
+                fullPath += "test1.txt";
+                break;
+            case 2:
+                fullPath += "test2.txt";
+                break;
+            case 3:
+                fullPath += "test3.txt";
+                break;
+            case 4:
+                fullPath += "test4.txt";
+                break;
+            case 5:
+                fullPath += "test5.txt";
+                break;
+            case 6:
+                fullPath += "test6.txt";
+                break;
+            default:
+                break;
         }
 
+        try {
+            service = new LexerService(fullPath);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        System.out.println("Reconhecimento dos tokens");
+
+        while (true) {
+            try {
+                Token token = service.scan();
+                if (token instanceof EndOfFile) {
+                    break;
+                } else {
+                    System.out.println(token.toString());
+                }
+            } catch (Exception e) {
+                System.out.println("Error with read file!");
+                break;
+            }
+        }
+        System.out.println("Tabela de Símbolos : ");
+        service.showSymbolsTable();
     }
 }
---
+++ src/lexer/models/EndOfFile.java
@@ -0,0 +1,18 @@
+package lexer.models;
+
+public class EndOfFile extends Token {
+    private int line;
+    private String lexeme;
+
+    public EndOfFile(int line, String lexeme) {
+        super(Tag.EOF);
+        this.line = line;
+        this.lexeme = lexeme;
+    }
+
+    public String toString() {
+        return "Final de Arquivo - Tag : " + Tag.EOF
+                + " linha : " + line
+                + " lexeme : " + lexeme;
+    }
+}
